b2a966d13323c5d206c90b77112f6842f4246afb
HeRedBo <hhbjkd@163.com>
2016-10-07 23:38:16 +0800

    node express 项目模拟数据展示

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 var express = require('express');
-var path = require('path');
+var path    = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var logger  = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views/pages')); // 指定视图文件路径
+//app.set('views', 'views/pages'); // 指定视图文件路径
+app.set('view engine', 'jade'); // 设置视图模板引擎
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
 
 var routes = require('./routes/index');
 var users  = require('./routes/users');
 var movie  = require('./routes/movie');
 var admin  = require('./routes/admin');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,41 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Imooc 首页',
+    movies: [
+        {
+            title: '机械战警',
+            _id: 1,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+        },
+        {
+            title: '机械战警',
+            _id: 2,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+        },
+        {
+            title: '机械战警',
+            _id: 3,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+        },
+        {
+            title: '机械战警',
+            _id: 4,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+        },
+        {
+            title: '机械战警',
+            _id: 5,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+        },
+        {
+            title: '机械战警',
+            _id: 6,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+        },
+    ]
+   });
 });
 
 module.exports = router;
